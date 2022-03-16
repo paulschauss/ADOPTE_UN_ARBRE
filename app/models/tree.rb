453554acd66f2f4208d16bcf27f9fa720b4607a1
Paul Schauss <95611329+paulschauss@users.noreply.github.com>
2022-03-16 15:40:36 +0100

    Update tree.rb

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,7 +1,7 @@
 class Tree < ApplicationRecord
   belongs_to :user
 
-  validates :price, presence: true
-  validates :quantity_per_year, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
-  validates :name, presence: true
+  #validates :price, presence: true
+  validates :quantity_per_year, inclusion: { in: [1, 2, 3, 4, 5] }
+  #validates :name, presence: true
 end
