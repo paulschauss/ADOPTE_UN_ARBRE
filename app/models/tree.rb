524b32dca9d1d98467e7b47d6e0526db5134149d
Josephine <josephine.lazzaris@gmail.com>
2022-03-16 14:46:04 +0100

    validates update

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,3 +1,11 @@
 class Tree < ApplicationRecord
   belongs_to :user
+
+  validates :price, presence: true
+  validates :quantity_per_year, presence: true
+  validates :address, presence: true
+  validates :fruit, presence: true
+  validates :description, presence: true
+  validates :short_description, presence: true
+  validates :name, presence: true
 end
