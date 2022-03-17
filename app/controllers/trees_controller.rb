74929ae4ca4002887a617cfd690e8c1824aa8908
Paul Schauss <p+github@schauss.fr>
2022-03-17 09:33:22 +0100

    dont mergeeee

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -6,4 +6,22 @@ class TreesController < ApplicationController
   def show
     @tree = Tree.find(params[:id])
   end
+
+  def new
+    @tree = Tree.new
+  end
+
+  def create
+    @tree = Tree.new(tree_params)
+    @tree.save
+
+    redirect_to tree_path(@tree)
+    #render :new
+  end
+
+  private
+
+  def tree_params
+    params.require(:tree).permit(:name, :address, :price, :quantity_per_year, :fruit, :description, :short_description)
+  end
 end
