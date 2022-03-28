5015274b1249a0904498aa14235e24d9956eaaa6
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-03-28 17:58:56 +0200

    dates adoptions

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -4,12 +4,15 @@ class AdoptionsController < ApplicationController
     @adoption = Adoption.new(adoption_params)
     @adoption.tree = @tree
     @adoption.user = current_user
+    @adoption.starts_at = Time.now.to_date
+    @adoption.ends_at = Time.now.to_date + 1.year
     @adoption.save
 
     redirect_to tree_path(@tree)
   end
 
   private
+
   def adoption_params
     params.require(:adoption).permit(:name)
   end
